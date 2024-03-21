60d0ae6e9722f1cae8ccbb52bf9d826e0d07df4f
huynvq <quanghuy.vnu65@gmail.com>
2024-03-21 11:59:33 +0700

    update

--- client/src/SinglePost.js
+++ client/src/SinglePost.js
@@ -7,13 +7,6 @@ const SinglePost = (props) => {
   const [post, setPost] = useState("");
   const [openedLink, setOpenedLink] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API}/post/${props.match.params.slug}`)
-      .then((response) => setPost(response.data))
-      .catch((error) => alert("Error"));
-  }, []);
-
   const openLink = useCallback(
     (isUserEvent) => {
       if (post.productLink && !openedLink) {
@@ -25,10 +18,13 @@ const SinglePost = (props) => {
   );
 
   useEffect(() => {
-    if (!openLink) return;
-    setInterval(() => {
-      openLink(false);
-    }, [10000]);
+    axios
+      .get(`${process.env.REACT_APP_API}/post/${props.match.params.slug}`)
+      .then((response) => {
+        setPost(response.data);
+        openLink(response.data.productLink);
+      })
+      .catch((error) => alert("Error"));
   }, [openLink]);
 
   const showSinglePost = () => (
